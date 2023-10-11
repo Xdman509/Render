45f1688d11a3a9ea82c7f2e8a91e368eefd554ab
Xdman509 <Xdman509@gmail.com>
2023-10-11 17:16:58 -0500

    weather

---
+++ Render/23WF_Express_Template/birds.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/bird',function(req,res,next);{
+    let birdData = 
+    {
+            "species": "cardinal",
+            "habitat": "woodland",
+            "food": "seeds, fruit,insects",
+    }
+});
---
+++ Render/23WF_Express_Template/weather.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/weather',function(req,res,next);{
+let weatherdata = 
+{
+    “Temp”:90.5,
+    “Unit”: “fahrenheit”,
+    “Cloudy”:false,
+    “PrecipitationChance”:0
+  }
+});
+module.exports = router;
